6a50babc253c45092abc0b7c05ee6234f7fe94f2
spongebobpants <delay516@gmail.com>
2022-01-17 16:44:03 +0900

    spring

---
+++ src/main/java/com/javaex/controller/hello.java
@@ -0,0 +1,14 @@
+package com.javaex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class hello{
+
+	@RequestMapping( "/hello")
+	public String hello(){
+		System.out.println("/hellospring/hello");
+		return "/WEB-INF/views/index.jsp";
+	}
+}
